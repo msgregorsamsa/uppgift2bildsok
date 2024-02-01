9e5a558298e43ce68f7d1f2c22b16323524a3c4a
Anna <142659843+msgregorsamsa@users.noreply.github.com>
2024-02-01 17:48:45 +0100

    moved class from div to forms,  kept on building on JS-part

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
+const apiKey = '42110634-489e3894e133cfb0dd6eef927'
+
+let searchbarForm = document.querySelector('#searchbar');
+
+searchbarForm.onSubmit = event => {
+    event.preventDefault();
+}
+readPictures()
+
 async function readPictures(){
+
+    let searchInput = searchbarForm.elements.searchbar.value;
 
     let response = await fetch(
                     'https://pixabay.com/api/' +
-                    '42110634-489e3894e133cfb0dd6eef927'
+                    '?key=' + apiKey +
+                    '&q=' + searchInput +
+                    '&image_type=photo'
                     );
 
                     let json = await response.json();
+
+                    displayResults(json.hits);
 }
 
-let searchInput = document.querySelector('#searchbar')
+function displayResults(response){
+    // Implementera hur bilderna visasa upp
+    return console.log(response)
+
+}
+
+
+
+
+
+
