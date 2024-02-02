6c2a5d8a95ae51251af8dda29eaa3877770ffc5d
Anna <142659843+msgregorsamsa@users.noreply.github.com>
2024-02-02 13:23:47 +0100

    Added displayImages function, removed # again where it gets elementsbyID. Start on fixing how the images are linked :)

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
 const apiKey = '42110634-489e3894e133cfb0dd6eef927'
 
 let formElement = document.querySelector('form')
-let searchbarForm = document.getElementById('#searchbar');
-let colorChoice = document.getElementById('#color-choice')
+let searchbarForm = document.getElementById('searchbar');
+let colorChoice = document.getElementById('color-choice')
 
-// Ska implementeras
 let searchResults = document.querySelector('.search-results');
 
-let previousButton = document.getElementById('#previous-button');
-let nextButton = document.getElementById('#next-button');
+let previousButton = document.getElementById('previous-button');
+let nextButton = document.getElementById('next-button');
 
 
 let searchInput = '';
 let page = 1;
 
-// searchbarForm.onSubmit = event => {
-//     event.preventDefault();
-// }
-// readPictures()
 
 async function readPictures(){
 
-    let searchInput = searchbarForm.value;
+    searchInput = searchbarForm.value;
 
     let response = await fetch(
                     'https://pixabay.com/api/' +
@@ -30,22 +25,50 @@ async function readPictures(){
                     '&image_type=photo'
                     );
 
-    let json = await response.json();
-    let result = data.result;
+    let data = await response.json();
+    let result = data.hits; 
+    
+    displayImages(result)
+}
 
-    result.map(result => {
+
+
+
+function displayImages(result){
+    //Rensar ut eventuella tidigare sökningar
+        searchResults.innerHTML = '';
+    
+    
+    //Går igenom sökresultatet.
+        result.map(result => { 
             
-    });
+            //Skapar en ny div för varje bildelement
+            let imageContainer = document.createElement('div');
+            imageContainer.className = 'image-container';
+
+            //Själva bilden skapas
+            let image = document.createElement('img');
+            image.src = result.previewURL;
+            image.alt = result.user + result.tags;
+            
+            //Anchors osv skapas
+            let imageLink = document.createElement('a');
+            imageLink.href = result.pageURL;
+            imageLink.target = '_blank'; //Om bilden öppnas görs det i en ny flik, vill vi ha det så?
+            imageLink.textContent = 'Se bilden på Pixabay';
+            
+            //lägger till ny divbehållare under sökresultaten.
+            imageContainer.appendChild(image);
+            imageContainer.appendChild(imageLink);
+            searchResults.appendChild(imageContainer);   
+            });
+ 
 }
 
-function displayResults(response){
-    // Implementera hur bilderna visasa upp
-    return console.log(response)
+formElement.addEventListener('submit', (event) =>{
+    event.preventDefault();
 
-}
-
-
-
-
+    readPictures();
+});
 
 
